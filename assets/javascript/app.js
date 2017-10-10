66ae6e0835c102648f63b4747eb22df7e8a27475
Bowen <bowenbrinegar@Bowens-MacBook-Pro.local>
2017-10-10 16:22:35 -0400

    basics down, lots of bugs, but enough to work off of

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -110,6 +110,8 @@ var pokeName;
 var pokeHealth;
 var pokeImage;
 var referenceId;
+var userHealth;
+var catchHealth;
 
 var referenceId;
 
@@ -197,6 +199,17 @@ $('#pokemonCollection').on("click", "button", function() {
   ref.on("value", function(snapshot) {
      var image1 = renderPoke(snapshot.val());
       $('#user').append(image1)
+// =======
+
+//     userHealth = snapshot.val().health
+
+//     var nameEntry = $('<h3>').text(snapshot.val().name)
+//     var healthEntry = $('<h2>').text(userHealth)
+//     var imageEntry = $("<img class='pokeBattle'>").attr("src", snapshot.val().image);
+
+//     $('#user').append(imageEntry, healthEntry, nameEntry)
+
+// >>>>>>> basics down, lots of bugs, but enough to work off of
   }, function (error) {
      console.log("Error: " + error.code);
   });
@@ -205,6 +218,12 @@ $('#pokemonCollection').on("click", "button", function() {
   $('#catch').empty();
   var image = renderPoke(opponent)
   $("#catch").append(image)
+// ===
+//   var nameEntry = $('<h3>').text(pokeName)
+//   var healthEntry = $('<h2>').text(catchHealth)
+//   var imageEntry = $("<img class='pokeBattle'>").attr("src", pokeImage)
+//   $("#catch").append(image)
+// >>>>>>> basics down, lots of bugs, but enough to work off of
 
   battleMode();
 })
@@ -254,11 +273,30 @@ function battleMode() {
   //     });
   // }
 
-// removes user pokemon from database
-  // if (playerLosses) {
-  //   var ref = firebase.database().ref(referenceId);
-  //   ref.remove()
-  // }
+  $('#attackButton').on("click", function() {
+      userHealth = userHealth - 10;
+      catchHealth = catchHealth - 10;
+
+      $('#catch h2').text(catchHealth);
+      $('#user h2').text(userHealth);
+
+      if (userHealth <= 0) {
+        var ref = firebase.database().ref(referenceId);
+        ref.remove()
+      };
+
+      $('#catchButton').on("click", function() {
+        if (catchHealth < 10 && catchHealth > 0) {
+          database.ref().push({ 
+            name: pokeName,
+            health: pokeHealth,
+            image: pokeImage
+          });
+        };
+      });   
+  });
+
+  
 
 // closes battle mode, potentially shows stats of pokemon collected
   // if (gameover) {
