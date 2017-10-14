be675dbc41cde1e8dbf8cf8a6f45dddd002689fc
Bowen <bowenbrinegar@Bowens-MacBook-Pro.local>
2017-10-14 13:20:29 -0400

    changed success and error functions

--- assets/javascript/login.js
+++ assets/javascript/login.js
@@ -8,8 +8,10 @@ $('#sumbit').on("click", function() {
    if (select == 2) {
       firebase.auth().createUserWithEmailAndPassword(email, password).then(function(success){
         alert("New User Created");
+        $('#userPortal').hide();
         userId = firebase.auth().currentUser;
       }).catch(function(error) {
+        alert("User Does Not Exist");
         var errorCode = error.code;
         var errorMessage = error.message;
       });
@@ -18,8 +20,10 @@ $('#sumbit').on("click", function() {
    if (select == 1) {
       firebase.auth().signInWithEmailAndPassword(email, password).then(function(success){
         alert("You're Logged In");
+        $('#userPortal').hide();
         userId = firebase.auth().currentUser;
       }).catch(function(error) {
+        alert("Invalid Email");
         var errorCode = error.code;
         var errorMessage = error.message;
       });
@@ -45,5 +49,5 @@ $('#sumbit').on("click", function() {
     }
 
 
-    $('#userPortal').hide();
+    
 });
