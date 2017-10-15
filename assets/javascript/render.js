fe3ae8e8017e2742174ca3a782ed75cf796fdc11
Kevin Gilland <kev.gilland93@gmail.com>
2017-10-15 00:18:23 -0400

    removed console meant for patch branch

--- assets/javascript/render.js
+++ assets/javascript/render.js
@@ -111,9 +111,6 @@ function loadPokemon() {
 
     ref.on("child_added", function(childSnapshot){
       var poke = getPokeValuesFromDB(childSnapshot)
-      console.log(
-        "Left this here, check out how many times it gets called" + 
-        " when a new pokemon is added.", poke)
       var dataObj;
 
       var div = renderPokeInPouch(poke)
